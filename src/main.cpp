53d556a7d7dcb6e53a70b74a2363004e1ebc1ce0
Ned Bingham <eab242@cornell.edu>
2020-06-23 22:04:06 -0400

    fixing path and name logic

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,19 @@ int main(int argc, char **argv)
 		parse::generic_t peg;
 		parse::grammar_t gram;
 		peg.load(argv[i], gram);
-		std::string name = argv[i];
-		if (name.substr(name.size()-4) == ".peg")
-			name = name.substr(0, name.size()-4);
 
-		std::ofstream header(name + ".h");
-		std::ofstream source(name + ".cpp");
+		std::string path = argv[i];
+
+		size_t slash = std::string::npos, dot = std::string::npos;
+		slash = path.rfind("/");
+		dot = path.rfind(".");
+		slash = slash == std::string::npos ? 0 : slash+1;
+		dot = dot == std::string::npos ? path.size() : dot;
+
+		std::string name = path.substr(slash, dot-slash);
+
+		std::ofstream header(path.substr(0, dot) + ".h");
+		std::ofstream source(path.substr(0, dot) + ".cpp");
 		export_grammar(gram, "parse", name, header, source);
 	}
 }
